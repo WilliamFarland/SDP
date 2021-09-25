aec0517d3877fdc3de5d034da748094377b562fd
WilliamFarland <william.farland@outlook.com>
2021-09-25 14:53:42 -0400

    imaged support added w/ sheet music png

--- GUI.py
+++ GUI.py
@@ -1,36 +1,45 @@
 # SDP Front End
 # 9/25/2021
 # William Farland
-from buttonLogic import *
+
 from tkinter import *
-#from tkinter.ttk import *
+# from tkinter.ttk import *
 import sys
+from PIL import ImageTk, Image
 
-# Part 1: Main Window Creation
-root = Tk()
-root.title('SDP - Team 22')
-# root.iconbitmap()
-# root.attributes('-fullscreen', True)
-root.geometry("400x400")
-
-# Add some pizaz to the styling...
-#style = Style()
-#style.configure('TButton', font =
-#               ('calibri', 10, 'bold', 'underline'),
-#                foreground = 'red')
+def menuOptions_1_logic(input):
+    if input == "Exit":
+        return 0
 
 
-menuOptions_1 = [
-    "Exit", "Save"
-]
+# ---------- Part 1: Widget Creation ----------
+
+# Main Window Creation
+root = Tk()  # Create initial root widget
+root.title('SDP - Team 22')  # Name root widget
+root.attributes('-fullscreen', True)  # Display in full screen
+# root.geometry("400x400")  # Can use this if we need to debug in smaller window
+# root.iconbitmap()  # Add icon file later
+
+# Canvas for Image creation
+canvas = Canvas(root, width=2000, height=1200)  # Create canvas inside root widget
+
+# Drop Down Menu Creation
+menuOptions_1 = ["Exit", "Save"]
 clicked = StringVar()
 clicked.set("File")
 dropMenu_1 = OptionMenu(root, clicked, *menuOptions_1)
+
+# Image selection
+img = Image.open("SheetMusic.png")  # PIL solution
+img = img.resize((3000, 1000))
+img = ImageTk.PhotoImage(img)
+# ---------- Part 2: Place widgets ----------
 dropMenu_1.grid(column=0, row=0)
-# button = Button(root, text="OK", command=menuOptions_1_logic)
-# button.pack()
+canvas.grid(column=0, row=1)  # Place Canvas using geometry manager
+canvas.create_image(0, 0, anchor=NW, image=img)
 
-
+# ---------- Part 3: Infinite Loop for responsive GUI ----------
 while 1:
     root.update()
     output = menuOptions_1_logic(clicked.get())
